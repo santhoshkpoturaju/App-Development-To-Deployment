fd274587e1cee87111c64f3f23c03351bbaa4df9
Santhosh Poturaju <psanthoshkumar08@gmail.com>
2025-09-13 14:21:07 -0700

    updated code

---
+++ src/helloworldsite/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def hello_world(request):
+    html_content = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Hello World Site</title>
+    </head>
+    <body>
+        <h1>Hello, World!</h1>
+        <p>Welcome to your Django Hello World site. this is version 3.0 app</p>
+    </body>
+    </html>
+    """
+    return HttpResponse(html_content)
